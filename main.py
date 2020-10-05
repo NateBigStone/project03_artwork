fb3d96a8f30812d6a88fd2eab26cec8abe0df351
NateBigStone <n.gaut@bigstonemgmt.com>
2020-10-04 20:27:01 -0500

    option six availability

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from menu import Menu
 import ui
 from database import *
+import sys
 
 
 def main():
@@ -23,6 +24,7 @@ def create_menu():
     menu_class.add_option('3', 'Search for Available Artworks by Artist', search_artist_available)
     menu_class.add_option('4', 'Add Artwork', add_artwork)
     menu_class.add_option('5', 'Delete Artwork', delete_artwork)
+    menu_class.add_option('6', 'Change Artwork Availability', availability_artwork)
     menu_class.add_option('T', 'Generate Test Tables', generate_test_tables)
     menu_class.add_option('D', 'Drop Test Tables', drop_test_tables)
     menu_class.add_option('Q', 'Quit', quit_program)
@@ -74,7 +76,7 @@ def add_artwork():
 
 def delete_artwork():
     try:
-        delete_art = ui.delete_artwork_info()
+        delete_art = ui.common_artwork_info()
         confirm = input(f'Delete "{delete_art.artwork_name}"? (y/n):\n').lower().strip()
         if confirm == 'y':
             if Artwork.delete_by_id(delete_art.id) > 0:
@@ -83,21 +85,34 @@ def delete_artwork():
         ui.message(e)
 
 
+def availability_artwork():
+    try:
+        cng_avail = ui.common_artwork_info()
+        confirm = input(f'Change "{cng_avail.artwork_name}" availability? (y/n):\n').lower().strip()
+        if confirm == 'y':
+            status = cng_avail.available is False
+            toggle = (Artwork.update({Artwork.available: status}).where(Artwork.id == cng_avail.id))
+            toggle.execute()
+            ui.message(f'{cng_avail.artwork_name} status updated.')
+    except Exception as e:
+        ui.message(e)
+
+
 def generate_test_tables():
     try:
-        new_artist_1 = Artist(artist_name='nate johnson', email='nate@nate.com')
+        new_artist_1 = Artist(artist_name='Harry Truman', email='accidental_prez@missouri.gov')
         new_artist_1.save()
         new_artist_2 = Artist(artist_name='Nate', email='coolguy123@yahoo.com')
         new_artist_2.save()
         new_artist_3 = Artist(artist_name='Nick Gant', email='nick86@aol.com')
         new_artist_3.save()
-        new_artwork_1 = Artwork(artwork_name='nate beautiful art', price=20.33, artist=2)
+        new_artwork_1 = Artwork(artwork_name='Da Bomb', price=19.45, artist=1)
         new_artwork_1.save()
         new_artwork_2 = Artwork(artwork_name='best code ever', price=1.30, artist=2)
         new_artwork_2.save()
         new_artwork_3 = Artwork(artwork_name='Sold Artwork', price=700.9, available=False, artist=2)
         new_artwork_3.save()
-        new_artwork_4 = Artwork(artwork_name='delete', price=700.9, available=False, artist=1)
+        new_artwork_4 = Artwork(artwork_name='just delete', price=700.9, available=False, artist=1)
         new_artwork_4.save()
     except Exception as e:
         ui.message(e)
@@ -105,6 +120,7 @@ def generate_test_tables():
 
 def drop_test_tables():
     db.drop_tables([Artist, Artwork])
+    sys.exit("Tables Deleted")
 
 
 def quit_program():
@@ -114,11 +130,9 @@ def quit_program():
 if __name__ == "__main__":
     main()
 
-#TODO:
 """
-change availability of an artwork
+TODO:
 tests
-
 refactor:
 make it so that the artist or artwork query isn't case sensitive
 validate input: artist email. 
